6b7f61d3e4fdf92e6e362be014e7f3e1b867e72d
DevJ <josegperezdev@gmail.com>
2025-07-19 18:07:17 -0600

    Adjustments to polybar

--- dot_config/polybar/executable_active_workspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Use i3-msg to get workspaces JSON and jq to find the focused workspace on this monitor
-workspace=$(i3-msg -t get_workspaces | jq -r --arg mon "$MONITOR" '.[] | select(.output == $mon and .focused == true) | .name')
-
-echo "$workspace"

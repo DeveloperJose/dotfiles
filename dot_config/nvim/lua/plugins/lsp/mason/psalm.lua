6bcea64c5149568bd6cf350a840893ae5803de36
DevJ <josegperezdev@gmail.com>
2025-12-04 03:17:50 -0700

    Sync

---
+++ dot_config/nvim/lua/plugins/lsp/mason/psalm.lua
@@ -0,0 +1,7 @@
+return {
+    -- filetypes = { 'php' },
+    -- cmd = { 'php7.4', './vendor/bin/psalm-language-server' },
+    -- init_options = {
+    --     error_level = 5, -- 1=info, 5=error only
+    -- },
+}

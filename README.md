a87b24176a13d0b5bcb3f3ee7c8a19fa8738a8cd
DevJ <josegperezdev@gmail.com>
2026-01-07 05:38:41 -0700

    Test update

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 ## Init
 ```bash
-sudo pacman -S git fish neovim hyprland
-sudo pacman -S sane simple-scan brave-bin chezmoi lazygit thunar
-sudo pacman -S npm nodejs ripgrep
-sudo pacman -S composer
-sudo pacman -S fd less wget curl nmap ntp numlockx openssh unzip wl-clipboard
-sudo pacman -S texlive-latexrecommended texlive-latexextra texlive-mathscience texlive-binextra texlive-bibtexextra zathura biber
+pacman -S ...
 
 git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia
 ~/.local/share/caelestia/install.fish
--- dot_local/private_share/caelestia/fish/functions/chadd.fish
+++ dot_local/private_share/caelestia/fish/functions/chadd.fish
@@ -1,3 +1,20 @@
-function chadd --description 'chezmoi re-add shortcut'
+function chadd --description 'chezmoi re-add, stage all, and commit with message'
+    # Check if a message was provided
+    if test (count $argv) -eq 0
+        echo "Error: Please provide a commit message."
+        echo "Usage: chadd \"your message here\""
+        return 1
+    end
+
+    # Run the sequence
     chezmoi re-add
+    
+    # Use chezmoi's source directory context to run git commands
+    set -l chezmoi_dir (chezmoi source-path)
+    
+    git -C $chezmoi_dir add -A
+    git -C $chezmoi_dir commit -m "$argv"
+    git -C $chezmoi_dir push
+    
+    echo "Changes committed and pushed to chezmoi source: $argv"
 end

f1a46afb5c7c6878ce65284bed5b64369eb03c06
DevJ <josegperezdev@gmail.com>
2026-01-07 22:16:55 -0700

    Sync

--- dot_local/private_share/caelestia/fish/functions/reload.fish
+++
@@ -1,5 +0,0 @@
-function reload --description 'Reload Fish shell and Hyprland'
-    hyprctl reload
-    source ~/.config/fish/config.fish
-    notify-send "Config Reloaded" "Hyprland and Fish are up to date." --icon=view-refresh
-end
